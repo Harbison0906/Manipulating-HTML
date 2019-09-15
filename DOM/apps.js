2eed533d7933996c0d35f0849c580491c0092e48
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-15 17:57:45 -0500

    added a form that alerts with whatever you submit in the text box.

--- DOM/apps.js
+++ DOM/apps.js
@@ -1,15 +1,40 @@
-let h3 = document.createElement('h3');
-let h3Text = document.createTextNode('Objective 1:')
-h3.appendChild(h3Text);
-document.body.appendChild(h3);
+let step1 = document.getElementById('step1');
+let step2 = document.getElementById('step2');
+let step3 = document.getElementById('step3');
+let step4 = document.getElementById('step4');
+let step5 = document.getElementById('step5');
+let step6 = document.getElementById('step6');
+
+let objective1 = document.createElement('h3');
+let text1 = document.createTextNode('Objective 1:');
+objective1.appendChild(text1);
+step1.appendChild(objective1);
+
 
 let btn = document.createElement('button');
-btn.id = 'button1'
+btn.id = 'button1';
 let btnText = document.createTextNode('Click me!');
 
+
 btn.appendChild(btnText);
-document.body.appendChild(btn);
+step1.appendChild(btn);
 
 btn.addEventListener('click', function() {
   alert('This is a nice alert message.');
 })
+
+let objective2 = document.createElement('h3');
+let text2 = document.createTextNode('Objective 2:');
+objective2.appendChild(text2);
+step2.prepend(objective2);
+
+let btnSubmit = document.getElementById('btnSubmit');
+
+btnSubmit.addEventListener('click', function() {
+let textField = document.getElementById('textField').value;
+  alert(textField);
+})
+
+
+
+
