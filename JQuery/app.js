7af6ac8cfc49a839dca1dbcf2e8c2bac237abbf6
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-22 17:00:52 -0500

    added text field with submit button. Alert window pulls with whatever is submitted in text field as message; then added div that changes color when moused over

--- JQuery/app.js
+++ JQuery/app.js
@@ -1,3 +1,6 @@
+// Objective 1 - a button that pulls an 
+// alert window with a nice message when clicked
+
 let step1 = $('<section></section');
 let heading1 = $('<h3>Objective 1:</h3>');
 $(step1).append(heading1);
@@ -8,5 +11,41 @@ $(step1).append(btn1);
 
 $(btn1).click(function() {
   alert('This is a very nice message, indeed.');
+});
+
+// Objective 2 - form with submit button 
+// that pulls alert window when clicked 
+// containing whatever is entered in the 
+// text field
+
+let step2 = $('<section></section');
+let heading2 = ('<h3>Objective 2:</h3>');
+$(step2).append(heading2);
+$('body').append(step2);
+
+let btn2 = $('#btnSubmit');
+let textField = $('#textField');
+$('body').append(textField);
+$('body').append(btn2);
+
+$(btn2).click(function() {
+  alert(textField.val());
 })
 
+// Objective 3 - Make a div change color when mousing over
+
+let step3 = $('<section></section>');
+let heading3 = $('<h3>Objective 3:</h3>');
+$(step3).append(heading3);
+$('body').append(step3);
+
+let div = $('<div></div>');
+$(div).attr('id', 'colorDiv');
+$(step3).append(div);
+
+$(div).mouseover(function() {
+  $(this).css('background-color', '#FFFFFF');
+});
+$(div).mouseleave(function() {
+  $(this).css('background-color', '');
+});
